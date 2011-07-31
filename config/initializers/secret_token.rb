9a91f5ab4b36136f4cba6e2cb5e400f043e13705
Naveen Bysani <naveen.bysani+github@gmail.com>
2011-07-31 04:27:57 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Glossyfish::Application.config.secret_token = '4212e48e2e32644397d751c1e4145ec8877ba638a72902c377687e3077b6f7ea6198f88ba5d775f3d672c29c28e7d94bff6e2348c11ce3d5f266d9711259d7e4'
